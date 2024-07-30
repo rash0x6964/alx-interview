6794c95a9d3a2aa2362394521141d68e7ff615bb
rash0x6964 <rash0x6964@gmail.com>
2024-07-30 10:52:09 +0100

    ...

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,16 +4,14 @@
 
 def canUnlockAll(boxes):
     """Determines if all the boxes can be opened."""
-    n = len(boxes)
-    opened_boxes = set()
-    keys = [0]
 
-    while keys:
-        key = keys.pop()
-        if key not in opened_boxes:
-            opened_boxes.add(key)
-            for k in boxes[key]:
-                if k not in opened_boxes:
-                    keys.append(k)
+    for key in range(1, len(boxes)):
+        flag = False
+        for box in range(len(boxes)):
+            if key in boxes[box] and box != key:
+                flag = True
+                break
+        if not flag:
+            return False
 
-    return len(opened_boxes) == n
+    return True
