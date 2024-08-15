940b01c21e760314ab5ac6606b2cb0f82468b90e
rash0x6964 <rash0x6964@gmail.com>
2024-08-15 13:38:57 +0100

    task 0: Log parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+""" Reads stdin line by line and computes metrics """
+
+import sys
+
+
+def print_stats(total_size, status_codes):
+    """ Prints the statistics. """
+
+    print(f"File size: {total_size}")
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print(f"{code}: {status_codes[code]}")
+
+
+def main():
+    """ Read/parce each line and accumulated statistics """
+
+    total_size = 0
+    line_count = 0
+    status_codes = {
+        200: 0,
+        301: 0,
+        400: 0,
+        401: 0,
+        403: 0,
+        404: 0,
+        405: 0,
+        500: 0
+    }
+
+    try:
+        for line in sys.stdin:
+            # print(">>>", line)
+            parts = line.split()
+            # print(len(parts), parts)
+
+            if len(parts) != 9:
+                continue
+
+            try:
+                file_size = int(parts[-1])
+                total_size += file_size
+            except ValueError:
+                continue
+
+            try:
+                status_code = int(parts[-2])
+                if status_code in status_codes:
+                    status_codes[status_code] += 1
+            except ValueError:
+                continue
+
+            line_count += 1
+
+            if line_count % 10 == 0:
+                print_stats(total_size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(total_size, status_codes)
+        raise
+
+    print_stats(total_size, status_codes)
+
+
+if __name__ == "__main__":
+    main()
