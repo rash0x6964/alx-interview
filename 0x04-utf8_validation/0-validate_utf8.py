a3e0a1601691f0360b9922ca4707788916d69099
rash0x6964 <rash0x6964@gmail.com>
2024-08-21 12:41:48 +0100

    task 0: UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+
+def validUTF8(data):
+    number_of_bytes = 0
+
+    mask1 = 1 << 7  # 10000000
+    mask2 = 1 << 6  # 01000000
+
+    for num in data:
+        mask = 1 << 7 # 10000000
+        if number_of_bytes == 0:
+            while mask & num:
+                number_of_bytes += 1
+                mask = mask >> 1
+
+            if number_of_bytes == 0:
+                continue
+
+            if number_of_bytes == 1 or number_of_bytes > 4:
+                return False
+        else:
+            if not (num & mask1 and not (num & mask2)):
+                return False
+
+        number_of_bytes -= 1
+
+    return number_of_bytes == 0
