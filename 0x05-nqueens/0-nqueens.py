094f51e5f01b897a64b18d41969eeb933add9ea9
rash0x6964 <rash0x6964@gmail.com>
2024-08-29 17:52:26 +0100

    N queens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+""" N queens puzzle """
+
+import sys
+
+
+def is_safe(board, row, col, n):
+    """Check if it's safe to place a queen at board[row][col].
+    """
+
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+
+def solve_nqueens_util(board, col, n, solutions):
+    """Recursively attempt to place queens on the chessboard.
+    """
+    if col >= n:
+        solution = []
+        for i in range(n):
+            for j in range(n):
+                if board[i][j] == 1:
+                    solution.append([i, j])
+        solutions.append(solution)
+        return True
+
+    res = False
+    for i in range(n):
+        if is_safe(board, i, col, n):
+            board[i][col] = 1
+            res = solve_nqueens_util(board, col + 1, n, solutions) or res
+            board[i][col] = 0  # Backtrack
+
+    return res
+
+
+def solve_nqueens(n):
+    """Solve the N-Queens problem and return all possible solutions.
+    """
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    solutions = []
+    solve_nqueens_util(board, 0, n, solutions)
+    return solutions
+
+
+def main():
+    """Main function to handle input, validate it,
+        and solve the N-Queens problem.
+    """
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solutions = solve_nqueens(n)
+    for solution in solutions:
+        print(solution)
+
+
+if __name__ == "__main__":
+    main()
