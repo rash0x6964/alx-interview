1bc273877c8a180ab730956a19fbb399a382ab8d
rash0x6964 <rash0x6964@gmail.com>
2024-10-02 16:31:25 +0100

    ...

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,48 +2,32 @@
 """ Prime Game """
 
 
-def generate_primes(n):
-    """Helper function to generate all primes
-        and their multiples for numbers up to n.
-    """
-    sieve = [True] * (n + 1)
-    sieve[0] = sieve[1] = False
-    primes = []
-    for i in range(2, n + 1):
-        if sieve[i]:
-            primes.append(i)
-            for multiple in range(i * i, n + 1, i):
-                sieve[multiple] = False
-    return primes
-
-
-def calculate_winner(n):
-    """Calculate the winner of a game round for a given n.
-    """
-    primes = generate_primes(n)
-    # Maria always plays first
-    moves = len(primes)
-    if moves % 2 == 0:
-        return "Ben"
-    else:
-        return "Maria"
-
-
 def isWinner(x, nums):
-    """Determine who the winner is after x rounds of the game.
-    """
+    if x < 1 or not nums:
+        return None
+
+    max_n = max(nums)
+
+    is_prime = [True] * (max_n + 1)
+    is_prime[0] = is_prime[1] = False
+
+    for i in range(2, int(max_n**0.5) + 1):
+        if is_prime[i]:
+            for multiple in range(i * i, max_n + 1, i):
+                is_prime[multiple] = False
+
+    primes_up_to = [0] * (max_n + 1)
+    for i in range(1, max_n + 1):
+        primes_up_to[i] = primes_up_to[i - 1] + (1 if is_prime[i] else 0)
+
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
-        if n == 1:
+        if primes_up_to[n] % 2 == 0:
             ben_wins += 1
         else:
-            winner = calculate_winner(n)
-            if winner == "Maria":
-                maria_wins += 1
-            else:
-                ben_wins += 1
+            maria_wins += 1
 
     if maria_wins > ben_wins:
         return "Maria"
